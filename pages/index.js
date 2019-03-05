de9e35d7cf7473b80c52dd0f97e628a139effdc1
Luis F. Escobar-Driver <lfescoba@uci.edu>
2019-03-05 11:04:07 -0800

    base commit
    have the skeleton I need. localhost website running

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import { Card, Button, Popup, Image, Dimmer, Header, Icon } from 'semantic-ui-react';
+// import Layout from '../components/Layout.js';
+import { Link } from '../routes';
+
+
+class CampaignIndex extends Component {
+
+    render() {
+        return(
+                
+        <h3>Working?</h3>
+
+        );
+    }
+}
+
+export default CampaignIndex;
---
+++ routes.js
@@ -0,0 +1,10 @@
+const routes = require('next-routes')();
+
+// routes
+//     //  ('url', 'location of file to display on url')
+//     .add('/campaigns/new', '/campaigns/new')
+//     .add('/campaigns/:address', '/campaigns/show')
+//     .add('/campaigns/:address/requests', '/campaigns/requests/index')
+//     .add('/campaigns/:address/requests/new', '/campaigns/requests/new');
+
+module.exports = routes;
