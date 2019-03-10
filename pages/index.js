f0119ca0ff774b93e7d01bb8c34694f13e4cf15b
Luis F. Escobar-Driver <lfescoba@uci.edu>
2019-03-10 16:18:40 -0700

    adding more projects from portfolio

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,9 @@ class CampaignIndex extends Component {
         return(
         <Layout>
             <div style={{"color":"white"}} as="h2" id="JS-Section">JS Section</div>
+            <div style={{"color":"white"}} id="Python-Section">Python Section</div>
+            <div style={{"color":"white"}} id="C++-Section">c++ section</div>
+            <div style={{"color":"white"}} id="Solidity-Section">sol section</div>
 
             <Grid divided="vertically">
 
@@ -67,11 +70,7 @@ class CampaignIndex extends Component {
                             <Image src="../static/assets/dPen.png"></Image>
                             <Card.Content>
                                 <Card.Header>Decentralized Kickstarter</Card.Header>
-                                <Card.Meta>
-                                    <Link route="https://decentalizedkickstarter.herokuapp.com">
-                                        <a target="_blank">Website</a>
-                                    </Link>
-                                </Card.Meta>
+                                <Card.Meta>Full stack dapp</Card.Meta>
                                 <Card.Description>A decentralized kickstarter live on the rinkey testnet.</Card.Description>
                                 <div style={{ marginTop: '10px' }}>
                                     <Link route="https://github.com/LEscobar-Driver/dKick">
@@ -96,6 +95,9 @@ class CampaignIndex extends Component {
                             <List.Item>
                                 Live on Rinkeby testnet.
                             </List.Item>
+                            <List.Item>
+                                Live at https://decentalizedkickstarter.herokuapp.com
+                            </List.Item>
                         </List>
 
                         <p style={{"color":"white"}}>
@@ -112,7 +114,6 @@ class CampaignIndex extends Component {
                     <Grid.Column>                    
                     </Grid.Column>
                 </Grid.Row>
-
 
 
                 <Grid.Row columns={4}>
@@ -141,27 +142,125 @@ class CampaignIndex extends Component {
                     <Grid.Column>
                         <List bulleted style={{"color":"white"}} as="h2">
                             <List.Item>
-                                Full stack ethereum dapp.
+                                Finished beginner to intermediate smart contract course.
                             </List.Item>
                             <List.Item>
-                                Ethereum Smart contract back end.
+                                Using 7 smart contracts. 5 custom contracts.
                             </List.Item>
                             <List.Item>
-                                React & Next js front end.
-                            </List.Item>
-                            <List.Item>
-                                Live on Rinkeby testnet.
+                                Solidaty, Web3.js & HTML
                             </List.Item>
                         </List>
 
                         <p style={{"color":"white"}}>
-                            I built & expanded this decentralized application. Users can create
-                            campaigns open to donations. Funds can only be withdrawn from campaigns
-                            upon withdraw requests being voted on by the users who contributed funds.
-                            A withdraw request must achieve 50% yes votes for funds to be withdrawn.
+                            Completed Making the Zombie Factory, Zombies Attack Their Victims, Advanced
+                            Solidity Concepts, Zombie Battle System, ERC721 & Crypto-Collectibles, & 
+                            App Front-ends & Web3.js
                         </p>
                     </Grid.Column>
                 </Grid.Row>
+
+
+                <Grid.Row columns={4}>
+                    <Grid.Column>
+                        <Card fluid>
+                            <Image size="small" centered src="../static/assets/depthFirst.png"></Image>
+                            <Card.Content>
+                                <Card.Header>Recursive Depth-First Algorithm</Card.Header>
+                                <Card.Meta>Tree based data structure</Card.Meta>
+                                <Card.Description>This algorithm is capable of solving any MxM & NxM 2-D maze</Card.Description>
+                                <div style={{ marginTop: '10px' }}>
+                                    <Link route="https://github.com/LEscobar-Driver/Code/tree/master/CPlusPlus/Maze">
+                                        <a target="_blank">Source Code</a>
+                                    </Link>
+                                </div>
+                            </Card.Content>
+                        </Card>
+                    </Grid.Column>
+
+                    <Grid.Column>
+                        <List bulleted style={{"color":"white"}} as="h2">
+                            <List.Item>
+                                C++ recursive depth-first algorithm.
+                            </List.Item>
+                            <List.Item>
+                                Algorithm is designed to generated a perfect maze. Algoritm is also 
+                                capable of solving any perfect maze. (Perfect means it has a solution)
+                            </List.Item>
+                            <List.Item>
+                                Data structures and trees.
+                            </List.Item>
+                        </List>
+
+                        <p style={{"color":"white"}}>
+                            A project I completed at the University of California, Irvine. This C++ algorithm is a 
+                            tree structure which is designed to traverse a structure depth-first recursively. It is
+                            an extremely efficient algorithm. Zero memory leaks; I received a perfect score. 
+                        </p>
+                    </Grid.Column>
+
+                    <Grid.Column>
+                    </Grid.Column>
+
+                    <Grid.Column>                    
+                    </Grid.Column>
+
+                </Grid.Row>
+
+
+                <Grid.Row columns={4}>
+                    <Grid.Column>
+                    </Grid.Column>
+
+                    <Grid.Column>                    
+                    </Grid.Column>
+
+                    <Grid.Column>
+                        <Card fluid>
+                            <Image size="medium" src="../static/assets/AI.png"></Image>
+                            <Card.Content>
+                                <Card.Header>Othello AI</Card.Header>
+                                <Card.Meta>Artificial Intelligence</Card.Meta>
+                                <Card.Description>An AI capable of playing against another AI or a human</Card.Description>
+                                <div style={{ marginTop: '10px' }}>
+                                    <Link route="https://github.com/LEscobar-Driver/Code/tree/master/CPlusPlus/Othello%20AI">
+                                        <a target="_blank">Source Code</a>
+                                    </Link>
+                                </div>
+                            </Card.Content>
+                        </Card>
+                    </Grid.Column>
+
+                    <Grid.Column>
+                        <List bulleted style={{"color":"white"}} as="h2">
+                            <List.Item>
+                                C++ tree structure.
+                            </List.Item>
+                            <List.Item>
+                                Heuristic search is capable of an N depth search (user choice).
+                            </List.Item>
+                            <List.Item>
+                                Algorithm employs a game tree heuristic search; the AI only generates the
+                                part of the game tree that is logical given the opponents move. This makes
+                                the AI extremely efficient at findings the next best choice. 
+                            </List.Item>
+                        </List>
+
+                        <p style={{"color":"white"}}>
+                            A project I completed at the University of California, Irvine. This C++ algorithm is a 
+                            recursive run-time depth-first heuristic search algorithm which stores moves on the run time stack. 
+                            This means that the ai reduces the amount of memory needed to choose a move. 
+                            This negates the need to build and store a game tree as all possible moves are on the run-time
+                            stack. It is an extremely efficient algorithm. 
+                        </p>
+                    </Grid.Column>
+
+                </Grid.Row>
+
+
+
+
+
 
 
                 <Grid.Row columns={4}>
@@ -191,10 +290,6 @@ class CampaignIndex extends Component {
                 </Grid.Row>
             </Grid>
 
-
-
-
-
             <Grid divided="vertically" stretched>
                 <Grid.Row columns={4}>
                     <Grid.Column>
@@ -205,47 +300,15 @@ class CampaignIndex extends Component {
                         <div id="Solidity-Section">sol section</div>
                     </Grid.Column>
                 </Grid.Row>
+
+
+
+
+
+
             </Grid>
-
-            {/* <div id="JS Section">JS Section</div>
-
-            <div id="Python Section">Python Section</div>
-
-            <div id="C++ Section">c++ section</div>
-
-            <div id="Solidity Section">sol section</div> */}
-
-            
-
-            <h3>In index.js</h3>
-            <h3>In index.js</h3>
-            <h3>In index.js</h3>
-            <h3>In index.js</h3>
-            <h3>In index.js</h3>
-            <h3>In index.js</h3>
-            <h3>In index.js</h3>
-            <h3>In index.js</h3>
-            <h3>In index.js</h3>
-            <h3>In index.js</h3>
-            <h3>In index.js</h3>
-            <h3>In index.js</h3>
-            <h3>In index.js</h3>
-            <h3>In index.js</h3>
-            <h3>In index.js</h3>
-            <h3>In index.js</h3>
-            <h3>In index.js</h3>
-            <h3>In index.js</h3>
-            <h3>In index.js</h3>
-            <h3>In index.js</h3>
-            <h3>In index.js</h3>
-            <h3>In index.js</h3>
-            <div id="middle">MIDDLE</div>
-
         </Layout>
-        
-
         );
     }
 }
-
 export default CampaignIndex;
